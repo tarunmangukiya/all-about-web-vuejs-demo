382e535a9680bfb1677e4d044dc574c89d725f35
Tarun Mangukiya <tarunmangukiya@hotmail.com>
2020-08-15 11:37:27 +0530

    feat: Navbar Search

---
+++ components/NavBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="dark">
+    <b-navbar-brand href="#">All About Web Search</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-nav-form @submit.prevent="onSearch">
+        <b-form-input v-model="q" size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+        <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+      </b-nav-form>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown right>
+          <!-- Using 'button-content' slot -->
+          <template v-slot:button-content>
+            <em>User</em>
+          </template>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      q: null
+    }
+  },
+
+  methods: {
+    onSearch() {
+      this.$router.push({
+        name: 'search',
+        query: {
+          q: this.q
+        }
+      })
+    }
+  }
+}
+</script>
--- pages/search.vue
+++ pages/search.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="container">
-    <ul class="list-unstyled row">
-      <li v-for="(item, index) in items.data" :key="index" class="col-3 mb-4">
-        <icon-template :item="item" />
-      </li>
-    </ul>
+  <div>
+    <nav-bar />
+    <div class="container mt-5">
+      <ul v-if="!$fetchState.pending" class="list-unstyled row">
+        <li v-for="(item, index) in items.data" :key="index" class="col-3 mb-4">
+          <icon-template :item="item" />
+        </li>
+      </ul>
+      <h4 v-else>Loading...</h4>
+    </div>
   </div>
 </template>
 
 <script>
+import NavBar from '@/components/NavBar.vue'
 import IconTemplate from '@/components/IconTemplate.vue'
 
 export default {
   components: {
+    NavBar,
     IconTemplate
   },
 
@@ -22,8 +28,12 @@ export default {
     }
   },
 
+  watch: {
+    '$route.query': '$fetch'
+  },
+
   async fetch() {
-    const response = await this.$axios.get('https://api.iconscout.com/v2/search')
+    const response = await this.$axios.get(`https://api.iconscout.com/v2/search?query=${this.$route.query.q}`)
     this.items = response.data.response.items
   }
 }
