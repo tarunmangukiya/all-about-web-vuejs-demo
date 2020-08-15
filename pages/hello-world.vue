6c60c0f868893015ae3ef46101badb0c7d4fe1de
Tarun Mangukiya <tarunmangukiya@hotmail.com>
2020-08-15 12:48:24 +0530

    feat: Hello World

---
+++ pages/hello-world.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<style>
+h1 {
+  color: blue;
+}
+</style>
